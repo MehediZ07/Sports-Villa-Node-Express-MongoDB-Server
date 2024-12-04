7aaa196f2d3ca86f76cb8280eb911e5bafe90b87
Mehedi Zaman <mehedi.zaman.professional@gmail.com>
2024-12-05 03:13:35 +0600

    Basic CURD oparetion update

--- index.js
+++ index.js
@@ -83,40 +83,7 @@ async function run() {
 
 
         // Users related apis
-        app.get('/users', async (req, res) => {
-            const cursor = userCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
-
-        app.post('/users', async (req, res) => {
-            const newUser = req.body;
-            console.log('creating new user', newUser);
-            const result = await userCollection.insertOne(newUser);
-            res.send(result);
-        });
-
-        app.patch('/users', async (req, res) => {
-            const email = req.body.email;
-            const filter = { email };
-            const updatedDoc = {
-                $set: {
-                    lastSignInTime: req.body?.lastSignInTime,
-                    hotoUrl: req.body?.photo,
-                    name: req.body?.name,
-                }
-            }
-
-            const result = await userCollection.updateOne(filter, updatedDoc);
-            res.send(result);
-        })
-
-        app.delete('/users/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await userCollection.deleteOne(query);
-            res.send(result);
-        })
+        
 
     } finally {
         // Ensures that the client will close when you finish/error
